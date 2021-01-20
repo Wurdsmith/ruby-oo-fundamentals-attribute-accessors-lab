78f1421bfb1e8cf3af88068df7a1d3cd1f9e7527
Wurdsmith <wurdsmithing@gmail.com>
2021-01-20 07:22:08 -0600

    commit tree 8facd3bd029581d38c1e09ca6a3814669f8c9714

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+class Cat
+    attr_accessor :name
+
+    def name=(name_given)
+        @name = name_given
+    end
+
+    def meow
+        puts "meow!"
+    end
+
+end
